60126b116c8b9d6526312fc88a30d15a065cf039
Vincent Borowiec <132088538+vinniboro@users.noreply.github.com>
2024-04-18 13:58:30 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.tsx
+import './index';
+import React, { useState } from 'react';
+import HomePage from './container/HomePage';
+import SearchBar from './components/SearchBar';
+import getBusinesses  from "./utils/yelpAPI"; // Import the searchBusinesses function
 
 function App() {
+  const [businesses, setBusinesses] = useState<any[]>([]); // State to hold businesses
+
+  // Function to handle search and update businesses state
+  const handleSearch = async (restaurant: string, location: string, sortBy: string) => {
+    try {
+      // Call the searchBusinesses function with the search terms and location
+      const businesses = await getBusinesses(restaurant, location, sortBy);
+      setBusinesses(businesses); // Update businesses state with returned businesses
+    } catch (error) {
+      console.error("Error searching businesses:", error); // Handle any errors that occur during the search
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Pass handleSearch function and businesses state down to SearchBar */}
+      <SearchBar handleSearch={handleSearch} />
+      {/* Pass businesses state down to HomePage */}
+      <HomePage businesses={businesses} />
     </div>
   );
 }
